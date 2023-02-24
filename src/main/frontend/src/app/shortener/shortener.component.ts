99bc747758445684fac4d95fc7c120034ff67417
Bohdan Koval <gftonot@gmail.com>
2023-02-24 11:59:59 +0200

    Added init frontend

---
+++ src/main/frontend/src/app/shortener/shortener.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shortener',
+  templateUrl: './shortener.component.html',
+  styleUrls: ['./shortener.component.css']
+})
+export class ShortenerComponent {
+
+}
