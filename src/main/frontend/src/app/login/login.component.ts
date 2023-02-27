1a112e3fb281089a017534c65c55a228a440dab9
Bohdan Koval <gftonot@gmail.com>
2023-02-27 13:54:23 +0200

    Added requests to create link, login, sign up

--- src/main/frontend/src/app/login/login.component.ts
+++ src/main/frontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import User from '../user';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +10,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user!: User;
+  message!: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   logIn(formUser: NgForm): void {
-    
+    this.user = formUser.value;
+    this.http.post('http://localhost:8080/api/login', this.user, { observe: 'response' }).subscribe((res) => {
+      this.router.navigate(['/app-shortener'])
+    }, (error) => {
+      if (error.status = 404) {
+        this.message = "Email or password incorrect !";
+      } else {
+        this.message = "Something went wrong !";
+      }
+    });
+  }
+
+  redirectToSignup():void {
+    this.router.navigate(['/app-signup'])
   }
 }
--- src/main/frontend/src/app/signup/signup.component.ts
+++ src/main/frontend/src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import User from '../user';
 
 @Component({
   selector: 'app-signup',
@@ -8,9 +10,26 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  constructor(private http: HttpClient) { }
+  user: User = {
+    email: "",
+    password: ""
+  };
+  message!: string;
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   signUp(formUser: NgForm): void {
-    
+    this.user.email = formUser.value.email;
+    this.user.password = formUser.value.password;
+
+    this.http.post('http://localhost:8080/api/users', this.user, { observe: 'response' }).subscribe((res) => {
+      this.router.navigate(['/app-shortener'])
+    }, (error) => {
+      if (error.status = 409) {
+        this.message = "Email is already exist !";
+      } else {
+        this.message = "Something went wrong !";
+      }
+    });
   }
 }
