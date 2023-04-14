d680b6b4d3cb3d34965c66ade81851426b54c655
Bohdan Koval <gftonot@gmail.com>
2023-04-14 20:26:11 +0300

    Disabled cors

--- src/main/java/com/linkshortener/config/SecurityConfig.java
+++ src/main/java/com/linkshortener/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://linkshort.tech", "*"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET" ,"POST" , "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
